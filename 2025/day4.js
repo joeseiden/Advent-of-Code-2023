328ce72e22e41720b2f1c5438a513e8f517bbba8
Joe Seiden <jseiden@atlassian.com>
2025-12-20 20:39:08 -0800

    AOC 2025 Day 4: Solve Part 1

---
+++ 2025/day4.js
@@ -0,0 +1,36 @@
+import { readFileSync } from "fs";
+import { allAdjacentElementsFlattened, splitInputStringToArray } from "../lib/index.js";
+
+const exampleInput = readFileSync('./inputs/day4_example.txt', 'utf8');
+const input = readFileSync('./inputs/day4.txt', 'utf8');
+
+const parseInput = (input) => {
+    return splitInputStringToArray(input)
+}
+
+const part1 = (input) => {
+    const parsedInput = parseInput(input);
+    
+    let reachableRollCount = 0;
+    for (let i = 0; i < parsedInput.length; i++) {
+        for (let j = 0; j < parsedInput[i].length; j++) {
+            const element = parsedInput[i][j];
+            if( element === '@' && allAdjacentElementsFlattened(parsedInput, [i, j]).filter(el => el === '@').length < 4) {
+                reachableRollCount++
+            }
+        }
+        
+    }
+    return reachableRollCount;
+}
+
+console.log(`Part 1 example solution: ${part1(exampleInput)}`);
+console.log(`Part 1 solution: ${part1(input)}`);
+
+const part2 = (input) => {
+    const parsedInput = parseInput(input);
+    return true;
+}
+
+console.log(`Part 2 example solution: ${part2(exampleInput)}`);
+console.log(`Part 2 solution: ${part2(input)}`);
